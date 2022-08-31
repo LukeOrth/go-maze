634516b2e2468d66a26e8b4f1f1562dbbbfff025
Luke C. Orth <lorth001@gmail.com>
2022-08-31 13:39:54 -0500

    Removing maze.json

--- server/maze.go
+++ server/maze.go
@@ -1,15 +1,15 @@
 package server
 
 import (
-    "encoding/json"
-    "errors"
-    "log"
-    "net/http"
-    "strconv"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/LukeOrth/go-maze/maze"
 	"github.com/gorilla/mux"
 )
+
 
 func JSONError(w http.ResponseWriter, err interface{}, code int) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
@@ -18,35 +18,12 @@ func JSONError(w http.ResponseWriter, err interface{}, code int) {
     json.NewEncoder(w).Encode(err)
 }
 
-func getParms(r *http.Request, params ...string) ([]interface{}, error) {
-    values := make([]interface{}, len(params))
-    var errString string
-
-    for _, p := range params {
-       if !r.URL.Query().Has(p) {
-           errString = errString + "missing parameter: %s"
-       }
-       values = append(values, r.URL.Query().Get(p))
-    }
-    if len(errString) > 0 {
-        return nil, errors.New(errString)
-    }
-    return values, nil
-}
-
 func generateMazeGET(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    params, err := getParms(r, "columns", "rows", "scale")
-    if err != nil {
-        JSONError(w, err, 400)
-    }
-    columns, _ := strconv.Atoi(params[0].(string))
-    rows, _ := strconv.Atoi(params[1].(string))
-    scale, _ := strconv.Atoi(params[2].(string))
-    if columns == 0 {
-        JSONError(w, "missing parameter: columns", 400)
-        return
-    }
+    q := r.URL.Query()
+    columns, _ := strconv.Atoi(q.Get("columns"))
+    rows, _ := strconv.Atoi(q.Get("rows"))
+    scale, _ := strconv.Atoi(q.Get("scale"))
     maze := maze.NewMaze(columns, rows, scale)
     json.NewEncoder(w).Encode(maze)
 }
